eed055d2b29d25baaff4452132629d745279f90b
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-16 14:06:18 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+/**
+ * main - print numbers less than 10
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int a;
-	for(a = 0; a <10; a++)
-	{	
+
+	for (a = 0; a < 10; a++)
+	{
 		printf("%d", a);
 	}
 	printf("\n");
