aa28044000d017385e1a98f546f998b5f08a694a
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-17 04:35:08 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
+/**
+ * main - combinations
+ *
+ * Return: Always 0
+ *
+ */
 
 int main(void)
 {
 	int first;
+	int second = 1;
+	int temp = 1;
 
 	for (first = 0; first < 9; first++)
 	{
-		int second;
-
-		for (second = 1; second < 10; second++)
+		for (; second < 10; )
 		{
-			putchar(first + '0');
-			putchar(second + '0');
-			putchar(',');
-			putchar(' ');
+			if (first != second)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			second++;
 		}
+		second = temp++;
 	}
 	putchar('\n');
 	return (0);
