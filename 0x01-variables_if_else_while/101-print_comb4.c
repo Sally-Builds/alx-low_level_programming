f9741bf791f49e98c1d9dcc54ef433cf4c94d054
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-18 03:12:11 -0700

    Phewgit add .! final combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,15 +13,17 @@ int main(void)
 	while (first <= 7)
 	{
 		int second = first + 1;
+
 		while (second <= 8)
 		{
 			int third = second + 1;
-			while (third <=9)
+
+			while (third <= 9)
 			{
 				putchar(first + '0');
 				putchar(second + '0');
 				putchar(third + '0');
-				if(first != 7)
+				if (first != 7)
 				{
 					putchar(',');
 					putchar(' ');
