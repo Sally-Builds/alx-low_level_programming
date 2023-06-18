0e2930ab92390c82c0bab5f75b74990ffc1482a2
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-18 02:59:52 -0700

    Phewgit add .! after so many trials-- combinations done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - combinations
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int first = 0;
+
+	while (first <= 7)
+	{
+		int second = first + 1;
+		while (second <= 8)
+		{
+			int third = second + 1;
+			while (third <=9)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(third + '0');
+				if(first != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				third++;
+			}
+			second++;
+		}
+		first++;
+	}
+
+	putchar('\n');
+	return (0);
+}
