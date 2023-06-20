816dd1145a40a28983da1bc5d5a4e9ea31be77a2
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-20 10:59:51 -0700

    I love you Absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/** 
+ * _abs - return the absolute number of the parameter
+ * @n: number
+ *
+ * Return: Always number
+ * */
+int _abs(int n)
+{
+	return abs(n);
+}
