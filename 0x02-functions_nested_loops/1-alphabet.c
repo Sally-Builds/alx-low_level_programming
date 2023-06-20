646c4e284e5e8c78ab3a9dbc25d60b472ccacfb7
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-20 07:33:26 -0700

    insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
-/** 
+/**
  * print_alphabet - prints all alphabets in lowercase
  *
  */
 void print_alphabet(void)
 {
 	char ch = 'a';
-	for(; ch <= 'z'; ch++)
+
+	for (; ch <= 'z'; ch++)
 	{
-		_putchar(ch);	
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
