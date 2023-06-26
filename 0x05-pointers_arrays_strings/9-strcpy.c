6eb1a9401a1f49ae7c6b83969212cbb8a20a2dbb
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-26 16:35:15 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strcpy - function to copy strings
+ * @desc: destination to cpy
+ * @src: source to copy from
+ *
+ * Return: copied string
+ */
+char *_strcpy(char *desc, char *src)
+{
+	return (strcpy(desc, src));
+}
