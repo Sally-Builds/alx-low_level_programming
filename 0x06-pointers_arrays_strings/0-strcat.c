0b6c90280214cfed775b4bf3dd5d70d7eba5d8a0
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-29 08:22:52 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _strcat - string concatenation
+ * @dest: destination
+ * @src: source
+ *
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
