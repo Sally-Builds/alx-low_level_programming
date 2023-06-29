05b20a6c75595ad149f6e58d60b0df1061f00c22
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-29 08:53:39 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,12 @@
 #include <string.h>
-
+/**
+ * _strncat - string concatenation n bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: n bytes
+ *
+ * Return: cocatenated string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	return (strncat(dest, src, n));
