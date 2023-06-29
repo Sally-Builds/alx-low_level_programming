0ffb4f1c0f8925d3d8e3668ad7be33a94f75dcb0
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-06-29 13:23:08 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,5 +9,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	return (strncpy(dest, src, n));
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
