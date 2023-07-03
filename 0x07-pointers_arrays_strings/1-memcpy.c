03e0a0d8473e33345f03ea1a5a93e19437cfbc4d
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-03 07:42:22 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,5 +9,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return (memcpy(dest, src, n));
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
