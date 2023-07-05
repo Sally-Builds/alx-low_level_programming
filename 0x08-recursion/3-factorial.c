f8a320b2d3b743fcfc216b5ed9a1671fef5c8077
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-05 03:46:06 -0700

    You mustn't be afraid to dream a little bigger darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+/**
+ * factorial - calculates the factorial of a given number
+ * @n: number to calculate the factorial
+ *
+ * Return: factorial of a number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
