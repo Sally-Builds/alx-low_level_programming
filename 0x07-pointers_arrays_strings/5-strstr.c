c6e49708aeae7f35c86e2a6dd733c57aaf3700a1
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-04 17:55:42 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+/**
+ * _strstr - a function that locates a substring
+ *
+ * @haystack: input string to search for matching substrings
+ * @needle: subtring
+ *
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+		haystack++;
+	}
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 void print_chessboard(char (*a)[8])
 {
 	while (*a != '\0')
 	{
-		char *x = **&a;
-		while (**a != '\0')
+		char *a_ptr = a;
+		while (**a_ptr != '\0')
 		{
-			putchar(**a);
+			_putchar(**a_ptr);
+			a_ptr++;
 
 		}
+		a++;
 	}
 }
