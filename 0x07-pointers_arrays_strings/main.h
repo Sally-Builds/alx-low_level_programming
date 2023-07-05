8e187e312f21b75eb8efe97d89cd94f99f29cd3f
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-04 18:01:58 -0700

    The line of life is a ragged diagonal between desire and duty

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
 char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
 #endif
