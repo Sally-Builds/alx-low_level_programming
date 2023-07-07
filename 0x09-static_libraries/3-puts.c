9e746a1f5edf99f27324aa0d6cb215b5c92d3e67
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-07 09:04:03 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,16 @@
-void _puts(char *s)
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - print string
+ * @str: string to print
+ */
+void _puts(char *str)
 {
+	unsigned int i;
 
+	for (i = 0; i < strlen(str); i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
 }
