571b6c8a82e61997ee53287f904a95c5ddb6aae1
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-06 20:53:59 -0700

    they say we can only use a portion of our brain, now that's when we are awake. When we are asleep we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,9 +9,17 @@ int checker(char *s, int inc, int dec);
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
-	return(checker(s, 0, len - 1));	
-}
 
+	return (checker(s, 0, len - 1));
+}
+/**
+ * checker - checks if a string is palindrome
+ * @s: string to check
+ * @inc: incrementor variable
+ * @dec: decrementor variable
+ *
+ * Return: Always 0 or 1
+ */
 int checker(char *s, int inc, int dec)
 {
 	if (*(s + inc) != *(s + dec))
