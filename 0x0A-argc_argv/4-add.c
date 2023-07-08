9b5396abc20b91a86b4093ca5b0d12a84761f616
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-07 18:16:40 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argc == 1)
+			return (0);
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result = result + atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
