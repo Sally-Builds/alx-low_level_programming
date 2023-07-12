698ef18c1b25a85c059b2e670cb8c4052fae9754
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-11 23:45:31 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 #include <stddef.h>
+/**
+ * str_concat - string concatenation
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: pointer to string copy
+ */
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int lenTotal, len1, len2, i, j;
 	char *s;
+
 	len1 = 0;
 	len2 = 0;
 	lenTotal = 0;
@@ -13,9 +21,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL || s2 ==  NULL)
 		return (NULL);
 
-	while (s1[len1])
+	while (s2 && s1[len1])
 		len1++;
-	while (s2[len2])
+	while (s2 && s2[len2])
 		len2++;
 
 	lenTotal = len1 + len2;
@@ -23,14 +31,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s == NULL)
 		return (NULL);
-
-	while(i < len1)
+	while (i < len1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
-	
-
 	while (i < lenTotal)
 	{
 		s[i] = s2[j];
