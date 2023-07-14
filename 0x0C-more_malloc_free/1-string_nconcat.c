bfdbe497022970ae8e178c63d7a080a6ff15556b
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-14 11:17:41 -0700

    concatenates

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,37 +1,41 @@
-#include "main.h"
+#include <string.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- *string_nconcat - a program that concatenates two strings
- *@s1: the first character
- *@s2: the second character
- *@n: the integer that has been unsigned
- *Return: the pointer to resulting string
+ * string_nconcat - concatenates two strings together based on the size
+ * @n: size
+ * @s1: first string
+ * @s2: second string
+ * Return: string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int len1, len2, i, j;
 	char *ptr;
-	unsigned int i = 0, j = 0, str1 = 0, str2 = 0;
 
-	while (s1 && s1[str1])
-		str1++;
-	while (s2 && s2[str2])
-		str2++;
-	if (n < str2)
-		ptr = malloc(sizeof(char) * (str1 + n + 1));
-	else
-		ptr = malloc(sizeof(char) * (str1 + str2 + 1));
-	if (!ptr)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n >= len2)
+		n = len2;
+
+	ptr = malloc(sizeof(char) * (len1 + n + 1)
+			;
+	if (ptr == NULL)
 		return (NULL);
-	while (i < str1)
-	{
-		ptr[i] = s1[ptr];
-		i++;
-	}
-	while (n < str2 && i < (str1 + n))
-		ptr[i++] = s2[j++];
-	while (n >= str2 && i < (str1 + str2))
-		ptr[i++] = s2[j++];
+
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		ptr[i++] = s2[j];
+
 	ptr[i] = '\0';
+
 	return (ptr);
 }
