9ee2c10f729c23b4c2bbe9b96e8fb7c99785dbdb
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-27 08:00:52 -0700

    print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,13 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 1;
+	size_t i = 0;
 
 	if (h == NULL)
 		return (0);
 
 	if (h->str == NULL)
-		printf("[0] (nil)\n");
+		printf("[%d] %s\n", 0, "(nil)");
 	else
 		printf("[%d] %s\n", h->len, h->str);
 
@@ -25,5 +25,6 @@ size_t print_list(const list_t *h)
 		return (1);
 	}
 
+	i++;
 	return (i + print_list(h->next));
 }
