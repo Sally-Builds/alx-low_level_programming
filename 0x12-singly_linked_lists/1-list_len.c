fd1bde6a1c033eb156226d17fb84369fd5b86e49
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-27 14:15:39 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - count nodes in a singly linked list
+ * @h: node head
+ * Return: length of singly linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	if (h->next == NULL)
+	{
+		return (1);
+	}
+
+	i++;
+	return (i + list_len(h->next));
+}
