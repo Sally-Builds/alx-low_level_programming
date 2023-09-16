58a19251cc7d58cfa7c1e9911ca482da2786d22f
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-15 21:22:45 -0700

    debugging

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * insert_dnodeint_at_index - inserts node at index
+ * @h: head of list
+ * @idx: index
+ * @n: number
+ *
+ * Return: NULL or pointer to newly inserted node
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
@@ -14,19 +22,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (ptr == NULL)
 		return (NULL);
 
+	ptr->n = n;
+	if (idx == 0)
+	{
+		ptr->next = *h;
+		ptr->prev = NULL;
+
+		(*h)->prev = ptr;
+		(*h) = ptr;
+		return (ptr);
+	}
+
 	tmp = *h;
 
 	while (tmp != NULL)
 	{
 		if (count == idx)
 		{
-			ptr->n = n;
 			ptr->next = tmp;
 			ptr->prev = tmp->prev;
+			tmp->prev->next = ptr;
 			tmp->prev = ptr;
-			
-			if (idx == 0)
-				(*h) = ptr;
 			return (ptr);
 		}
 		tmp = tmp->next;
