2d119a548296215314f277a219e8bc5c9a9ea177
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-10-07 15:02:40 -0700

    debugging empty hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,12 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_create - create a hash table
+ *
+ * @size: size of the array in the hashtable
+ *
+ * Return: returns the pointer to the newly created hashtable
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
@@ -11,6 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
+	ht->size = size;
 	ht->array = malloc(size * sizeof(hash_node_t));
 
 	for (i = 0; i < size; i++)
