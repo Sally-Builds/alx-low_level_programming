5b9c162fa3faaba9a4295a0201b35063aae9b865
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-10-07 14:59:49 -0700

    empty hashtable

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = malloc(size * sizeof(hash_node_t));
+
+	for (i = 0; i < size; i++)
+	{
+		ht->array[i] = NULL;
+	}
+
+	return (ht);
+}
