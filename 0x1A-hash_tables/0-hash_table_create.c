d63c6f59000bcddbbe160eb714c6396580d9e367
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-10-07 15:08:56 -0700

    debugging

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
-	ht->size = size;
 	ht->array = malloc(size * sizeof(hash_node_t));
 
 	if (ht->array == NULL)
@@ -28,6 +27,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
+	ht->size = size;
 
 	for (i = 0; i < size; i++)
 	{
