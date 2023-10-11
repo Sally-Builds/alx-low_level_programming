ee3b24d8ad3965b98d107a95414200dce0e570a2
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-10-11 13:36:34 -0700

    hash delete

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,14 +15,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	/* Allocate memory for hash table structure */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
-	/* Allocate memory for array of hash nodes */
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
@@ -32,7 +30,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 
-	/* Initialize all hash node pointers to NULL */
 	for (inno = 0; inno < size; inno++)
 	{
 		hash_table->array[inno] = NULL;
