0a1dd0202d0f92ec28eeee16238a71f5b0a724ec
root <uzoagulujoshua@yahoo.com>
2023-12-01 21:47:07 -0800

    debug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,10 @@
 #include "hash_tables.h"
-
-
+/**
+ * hash_table_create - create a hash table
+ * @size: the size of the hash table
+ *
+ * Return: pointer to hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
@@ -12,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t));
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
 
 	if (table->array == NULL)
 		return (NULL);
