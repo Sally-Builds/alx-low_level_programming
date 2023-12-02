5d61a79016e9845f25ac835f73c10d1d18a03daa
root <uzoagulujoshua@yahoo.com>
2023-12-01 22:09:14 -0800

    debug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->size = size;
 	table->array = malloc(size *  sizeof(hash_node_t *));
 
 	if (table->array == NULL)
@@ -26,6 +25,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
+	table->size = size;
 	for (i = 0; i < table->size; i++)
 	{
 		table->array[i] = NULL;
