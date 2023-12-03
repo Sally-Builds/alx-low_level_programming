59a87d0784c3132871688cf77b484511de67aaa9
root <uzoagulujoshua@yahoo.com>
2023-12-03 15:54:36 -0800

    debugging

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *ht_item;
 	hash_node_t *current;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
