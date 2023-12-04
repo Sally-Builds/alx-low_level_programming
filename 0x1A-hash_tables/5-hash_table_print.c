5c46749dab84d04914a25f8fcbb539b2616b610a
root <uzoagulujoshua@yahoo.com>
2023-12-04 01:27:16 -0800

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	if (ht == NULL)
+		exit(0);
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (ht->array[i]->next == NULL)
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			else
+			{
+				current = ht->array[i];
+				while (current)
+				{
+					printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+					current = current->next;
+				}
+			}
+			if (i != (ht->size - 1))
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
