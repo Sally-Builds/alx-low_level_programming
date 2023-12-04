df7b12d7369b4d48de8c6d9a89c095b8b61e7aea
root <uzoagulujoshua@yahoo.com>
2023-12-04 01:29:24 -0800

    debug

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,9 +1,15 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - print hash table
+ * @ht: hash table to print
+ *
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *current;
+
 	if (ht == NULL)
 		exit(0);
 
