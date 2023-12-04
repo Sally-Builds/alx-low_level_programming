1b3c59e4aadca9debd323c938f7e930c95a689df
root <uzoagulujoshua@yahoo.com>
2023-12-04 02:07:01 -0800

    delete ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hash table from memory
+ * @ht: hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current, *next;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				next = current->next;
+				free(current->key);
+				free(current->value);
+				free(current);
+				current = next;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
